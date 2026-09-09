5a6050066dd7358194ba24a1c13873a57ac43307
vgnz93hs <ilce6400.mail@icloud.com>
2026-09-09 21:51:40 +0900

    feature: ファイルの整理。

---
+++ RC Line/Reader/ToolbarView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  RC Line
+//
+//  Created by vgnz93hs on 2026/09/09.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
