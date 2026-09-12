2feac8c4e749c00266c449b8f10c99694f504369
vgnz93hs <ilce6400.mail@icloud.com>
2026-09-12 15:47:38 +0900

    feature: 設定画面にインストラクションビューのボタンを設置。

--- RC Line/Instruction/InformationView.swift
+++ RC Line/Instruction/InformationView.swift
@@ -3,27 +3,9 @@ import SwiftUI
 struct InformationView: View {
     @Environment(\.openURL) var openURL
 
-    @State private var showInstructionView: Bool = false
-
     var body: some View {
         NavigationStack {
             List {
-                Section {
-                    Button(
-                        action: {
-                            showInstructionView = true
-                        },
-                        label: {
-                            Label("show_tutorial", systemImage: "lightbulb")
-                                .foregroundStyle(Color(.label))
-                        }
-                    )
-                    .accessibilityLabel(Text("show_tutorial"))
-                    .accessibilityHint(Text("opens_tutorial"))
-                    .accessibilityAddTraits(.isButton)
-                    .frame(alignment: .center)
-                }
-
                 Section("version_info") {
                     if let version = Bundle.main.object(
                         forInfoDictionaryKey: "CFBundleShortVersionString") as? String
@@ -34,15 +16,6 @@ struct InformationView: View {
             }
             .navigationTitle("info")
             .navigationBarTitleDisplayMode(.inline)
-            .sheet(
-                isPresented: $showInstructionView,
-                content: {
-                    InstructionView(showInstructionView: $showInstructionView)
-                        .accentColor(Color(.label))
-                        .presentationDetents([.large])
-                        .interactiveDismissDisabled(true)
-                }
-            )
         }
     }
 }
